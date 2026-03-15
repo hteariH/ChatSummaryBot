cc0867dd6a6b48bf4559da745ba559a25f25f9fb
htearih <aberkunsky@gmail.com>
2026-03-15 14:26:11 +0100

    Set default value for `spring.application.name` and improve Loki URL condition handling in logging config; add startup log message

--- src/main/kotlin/com/chatsummary/bot/ChatSummaryBotApplication.kt
+++ src/main/kotlin/com/chatsummary/bot/ChatSummaryBotApplication.kt
@@ -1,5 +1,6 @@
 package com.chatsummary.bot
 
+import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.retry.annotation.EnableRetry
@@ -11,5 +12,7 @@ import org.springframework.scheduling.annotation.EnableScheduling
 class ChatSummaryBotApplication
 
 fun main(args: Array<String>) {
+    val logger = LoggerFactory.getLogger(ChatSummaryBotApplication::class.java)
+    logger.info("Starting ChatSummaryBot Application...")
     runApplication<ChatSummaryBotApplication>(*args)
 }
